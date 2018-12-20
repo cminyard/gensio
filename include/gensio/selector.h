6590037abe69b064f049e0cef147efc56403f2d5
Corey Minyard <cminyard@mvista.com>
2018-12-20 15:37:16 -0600

    Make the license dual GPL and BSD.
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/gensio/selector.h
+++ include/gensio/selector.h
@@ -1,34 +1,13 @@
 /*
- * selector.h
+ *  gensio - A library for abstracting stream I/O
+ *  Copyright (C) 2018  Corey Minyard <minyard@acm.org>
  *
- * MontaVista IPMI interface code for timers and file waiting.
- *
- * Author: MontaVista Software, Inc.
- *         Corey Minyard <minyard@mvista.com>
- *         source@mvista.com
- *
- * Copyright 2002,2003 MontaVista Software Inc.
- *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2 of
- *  the License, or (at your option) any later version.
- *
- *
- *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
- *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- *  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- *  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- *  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this program; if not, write to the Free
- *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.  These licenses are available
+ *  in the root directory of this package named COPYING.LIB and
+ *  COPYING.BSD, respectively.
  */
 
 #ifndef SELECTOR
--- telnet.h
+++ telnet.h
@@ -1,3 +1,14 @@
+/*
+ *  gensio - A library for abstracting stream I/O
+ *  Copyright (C) 2018  Corey Minyard <minyard@acm.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.  These licenses are available
+ *  in the root directory of this package named COPYING.LIB and
+ *  COPYING.BSD, respectively.
+ */
 
 #ifndef _SER2NET_TELNET_H
 #define _SER2NET_TELNET_H
