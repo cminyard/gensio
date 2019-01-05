98e87cc6ca96163e6b428f5551cda532d719ad98
Corey Minyard <cminyard@mvista.com>
2019-01-04 18:38:49 -0600

    Fix some missed license changes.
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/gensio/gensio_selector.h
+++ include/gensio/gensio_selector.h
@@ -2,12 +2,19 @@
  *  gensio - A library for abstracting stream I/O
  *  Copyright (C) 2018  Corey Minyard <minyard@acm.org>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.  These licenses are available
- *  in the root directory of this package named COPYING.LIB and
- *  COPYING.BSD, respectively.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
  */
 
 #ifndef GENSIO_SELECTOR_H
