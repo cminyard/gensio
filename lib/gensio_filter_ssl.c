0f2115eaf7fa0fe8438c0c166c089f80e99350f9
Corey Minyard <cminyard@mvista.com>
2019-12-10 13:57:07 -0600

    ssl: Remove unnecessary checks for BIO errors
    The previous checks were not necessary, the actual error was elsewhere.
    Remove the checks.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- lib/gensio_filter_ssl.c
+++ lib/gensio_filter_ssl.c
@@ -431,9 +431,7 @@ ssl_ul_write(struct gensio_filter *filter,
 			     sizeof(sfilter->xmit_buf));
 
 	if (rdlen <= 0) {
-	    if (!BIO_should_retry(sfilter->io_bio) &&
-			!BIO_should_read(sfilter->io_bio) &&
-			!BIO_should_write(sfilter->io_bio)) {
+	    if (!BIO_should_retry(sfilter->io_bio)) {
 		gssl_log_err(sfilter, "Failed BIO read");
 		err = GE_COMMERR;
 		rdlen = 0;
@@ -471,9 +469,7 @@ ssl_ll_write(struct gensio_filter *filter,
 	int wrlen = BIO_write(sfilter->io_bio, buf, buflen);
 
 	if (wrlen <= 0) {
-	    if (BIO_should_retry(sfilter->io_bio) &&
-			!BIO_should_read(sfilter->io_bio) &&
-			!BIO_should_write(sfilter->io_bio)) {
+	    if (!BIO_should_retry(sfilter->io_bio)) {
 		gssl_log_err(sfilter, "Failed BIO write");
 		err = GE_COMMERR;
 		wrlen = buflen;
