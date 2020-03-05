cminyard/gensio

commit 4a0a0aa600d42d042624806fb39aa1585886362c

Use AX_PTHREAD to autodetect pthread support
Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

configure.ac
@@ -63,8 +63,11 @@ AC_ARG_WITH(pthreads,
 )
 
 if test "x$use_pthreads" != "xno"; then
-   LIBS="$LIBS -lpthread"
-   AC_DEFINE([USE_PTHREADS], [], [Enable multithreaded support])
+   AX_PTHREAD
+   if test "x$use_pthreads$ax_pthread_ok" = "xyesno"; then
+      AC_MSG_FAILURE([--with-pthreads was given, but no working pthread library was found])
+   fi
+   AM_CONDITIONAL([USE_PTHREADS], [test "x$ax_pthread_ok" = "xyes"])
 fi
 
 AC_ARG_WITH(openipmiflags,
@@ -88,7 +91,7 @@ if test "x$tryopenipmi" != "xno"; then
 fi
 
 if test "x$HAVE_OPENIPMI" != "xno"; then
-   if test "x$use_pthreads" == "xyes"; then
+   if test "x$ax_pthread_ok" == "xyes"; then
       LIBS="$LIBS -lOpenIPMI -lOpenIPMIpthread -lOpenIPMIutils"
    else
       LIBS="$LIBS -lOpenIPMI -lOpenIPMIposix -lOpenIPMIutils"

commit 8c008d596c30f65c679e063097f99a03cc6f7e01

Fix pthread autoconf detection and setup
A recent change to use ax_pthread.m4 had some issues with actually
enabling pthread support.  Set the proper variables and use AC_DEFINE,
no AM_CONDITIONAL, as AM_CONDITIONAL doesn't add anything to config.h.

Fixes: 4a0a0aa600d4 "Use AX_PTHREAD to autodetect pthread support"
Signed-off-by: Corey Minyard <cminyard@mvista.com>

configure.ac
@@ -53,8 +53,10 @@ AC_ARG_WITH(sctp,
 )
 
 use_pthreads=yes
+use_pthreads_set=false
 AC_ARG_WITH(pthreads,
 [  --with-pthreads=yes|no      Use pthreads or not.],
+    use_pthreads_set=true
     if test "x$withval" = "xyes"; then
       use_pthreads=yes
     elif test "x$withval" = "xno"; then
@@ -63,11 +65,14 @@ AC_ARG_WITH(pthreads,
 )
 
 if test "x$use_pthreads" != "xno"; then
-   AX_PTHREAD
-   if test "x$use_pthreads$ax_pthread_ok" = "xyesno"; then
-      AC_MSG_FAILURE([--with-pthreads was given, but no working pthread library was found])
-   fi
-   AM_CONDITIONAL([USE_PTHREADS], [test "x$ax_pthread_ok" = "xyes"])
+   AX_PTHREAD(
+      [LIBS="$LIBS $PTHREAD_LIBS"
+       CFLAGS="$CFLAGS $PTHREAD_CFLAGS"
+       CC="$PTHREAD_CC"
+       AC_DEFINE([USE_PTHREADS], [], [Enable multithreaded support])],
+      [if $use_pthreads_set; then
+          AC_MSG_FAILURE([--with-pthreads was given, but no working pthread library was found])
+       fi])
 fi
 
 AC_ARG_WITH(openipmiflags,

lib/gensio_osops.c
@@ -1905,17 +1905,9 @@ int
 gensio_os_scan_netaddr(struct gensio_os_funcs *o, const char *str, bool listen,
 		       int gprotocol, struct gensio_addr **raddr)
 {
-    int family = AF_UNSPEC, protocol, socktype;
+    int protocol, socktype;
     bool is_port_set;
     int rv;
-
-    if (strncmp(str, "ipv4,", 5) == 0) {
-	family = AF_INET;
-	str += 5;
-    } else if (strncmp(str, "ipv6,", 5) == 0) {
-	family = AF_INET6;
-	str += 5;
-    }
 
     switch (gprotocol) {
     case GENSIO_NET_PROTOCOL_TCP:
@@ -1938,15 +1930,13 @@ gensio_os_scan_netaddr(struct gensio_os_funcs *o, const char *str, bool listen,
 #endif
 
     case GENSIO_NET_PROTOCOL_UNIX:
-	if (family != AF_UNSPEC)
-	    return GE_INVAL;
 	return gensio_scan_unixaddr(o, str, raddr);
 
     default:
 	return GE_INVAL;
     }
 
-    rv = scan_ips(o, str, listen, family, socktype, protocol,
+    rv = scan_ips(o, str, listen, AF_UNSPEC, socktype, protocol,
 		  &is_port_set, true, raddr);
     if (!rv && !listen && !is_port_set)
 	rv = GE_INVAL;

tests/oomtest.c
@@ -1188,6 +1188,7 @@ main(int argc, char *argv[])
 #ifdef USE_PTHREADS
     pthread_t loopth[3];
     struct gensio_waiter *loopwaiter[3];
+    unsigned int num_extra_threads = 3;
 #endif
     unsigned int i, j;
     unsigned long errcount = 0;
@@ -1196,7 +1197,6 @@ main(int argc, char *argv[])
     sigset_t sigs;
     int testnr = -1, numtests = 0, testnrstart = -1, testnrend = MAX_LOOPS;
     gensio_time zerotime = { 0, 0 };
-    unsigned int num_extra_threads = 3;
 
 #ifndef ENABLE_INTERNAL_TRACE
     fprintf(stderr, "Internal tracing disabled, cannot run oomtest\n");
@@ -1252,6 +1252,7 @@ main(int argc, char *argv[])
 		exit(1);
 	    }
 	    testnrstart = strtol(argv[i], NULL, 0);
+#ifdef USE_PTHREADS
 	} else if (strcmp(argv[i], "-n") == 0) {
 	    i++;
 	    if (i >= argc) {
@@ -1259,6 +1260,7 @@ main(int argc, char *argv[])
 		exit(1);
 	    }
 	    num_extra_threads = strtol(argv[i], NULL, 0);
+#endif
 	} else if (strcmp(argv[i], "-e") == 0) {
 	    i++;
 	    if (i >= argc) {