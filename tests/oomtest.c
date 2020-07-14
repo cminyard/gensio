5b0c16d4850f063baba751c865a49c8955144557
Corey Minyard <cminyard@mvista.com>
2020-07-14 12:40:02 -0500

    tests:oom: Change mux test back to SCTP
    With the SCTP issue tracked down, the mux test can run on SCTP again.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- tests/oomtest.c
+++ tests/oomtest.c
@@ -151,14 +151,7 @@ struct oom_tests oom_tests[] = {
       .check_done = 1, .check_value = HAVE_LIBSCTP },
     { "udp,localhost,", "udp,0",
       .allow_pass_on_oom = true },
-    /*
-     * I would like this to run over SCTP, but there appears to be a
-     * bug in the Linux SCTP stack where I'm getting a 200ms delay
-     * every once in a while, enough to make it take a long time.
-     * This doesn't happen on the straight SCTP test, so it's some way
-     * the mux code is interacting with SCTP.  No problem with TCP.
-     */
-    { "mux,tcp,localhost,", "mux,tcp,0",
+    { "mux,sctp,localhost,", "mux,sctp,0",
       .check_done = 1, .check_value = HAVE_LIBSCTP },
     { "telnet(rfc2217),tcp,localhost,", "telnet(rfc2217),tcp,0" },
     { "serialdev,%s,115200", NULL,
