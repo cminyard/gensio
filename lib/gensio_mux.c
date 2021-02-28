cminyard/gensio

commit ea5e5970eecbc6b7a9c1eed875b2ba9b90e00981

mux: Fix a close/free issue
If the connection goes down when a channel is in MUX_INST_CLOSED or
MUX_INST_IN_REM_CLOSE, it would not free the channel and thus never free
the mux.  Add the proper deref for the channel.

Signed-off-by: Corey Minyard <cminyard@mvista.com>

lib/gensio_mux.c
@@ -1830,6 +1830,7 @@ mux_shutdown_channels(struct mux_data *muxdata, int err)
 
 	case MUX_INST_CLOSED:
 	case MUX_INST_IN_REM_CLOSE:
+	    chan_deref(chan); /* Will free it. */
 	    break;
 
 	case MUX_INST_PENDING_OPEN:

commit 4c7b348bd8297cd1a2bca2119b3f50c432700587

mux: Fix a refcount issue causing a free to never happen
A previous fix for this added a deref on shutdown, but that was
incorrect.  Remove that fix.

The basic problem that was happening was if you called gensio_free() on
a mux that was in a remove shutdown, it would never run through the
close code for mux channels, so it would not get the refcount
decremented from the close operation.

So, in the free operation, if we are in remote close state, do the
deref.

Fixes: ea5e5970ee mux: Fix a close/free issue
Signed-off-by: Corey Minyard <cminyard@mvista.com>

lib/gensio_mux.c
@@ -1246,8 +1246,14 @@ muxc_free(struct mux_inst *chan)
     if (--chan->freeref > 0)
 	goto out_unlock;
     switch (chan->state) {
-    case MUX_INST_IN_CLOSE:
     case MUX_INST_IN_REM_CLOSE:
+	/*
+	 * We don't call finish_close() in this case, so we need to do
+	 * the deref here instead.
+	 */
+	chan_deref(chan);
+	/* Fallthrough */
+    case MUX_INST_IN_CLOSE:
     case MUX_INST_IN_CLOSE_FINAL:
     case MUX_INST_IN_OPEN_CLOSE:
 	chan->close_done = NULL;
@@ -1830,7 +1836,6 @@ mux_shutdown_channels(struct mux_data *muxdata, int err)
 
 	case MUX_INST_CLOSED:
 	case MUX_INST_IN_REM_CLOSE:
-	    chan_deref(chan); /* Will free it. */
 	    break;
 
 	case MUX_INST_PENDING_OPEN:

c++/tests/basic_cpp_test.cc
@@ -248,6 +248,7 @@ private:
 
     void new_connection(Accepter *acc, Gensio *g) override
     {
+	io = g;
 	g->set_event_handler(ev);
 	ev = NULL;
 	g->set_read_callback_enable(true);
@@ -260,6 +261,7 @@ private:
 	waiter->wake();
     }
 
+    Gensio *io;
     Waiter *waiter;
     Event *ev;
 };