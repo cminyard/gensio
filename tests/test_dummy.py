8b1febd11e71735e21e7f30e0e274ac697ec995e
Corey Minyard <cminyard@mvista.com>
2021-10-16 15:52:23 -0500

    tests: Add a test for the dummy accepter
    Just to make sure it works, which it didn't.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

---
+++ tests/test_dummy.py
@@ -0,0 +1,20 @@
+#
+#  gensio - A library for abstracting stream I/O
+#  Copyright (C) 2018  Corey Minyard <minyard@acm.org>
+#
+#  SPDX-License-Identifier: GPL-2.0-only
+#
+
+import utils
+import gensio
+
+print("Test dummy")
+
+acc = gensio.gensio_accepter(utils.o, "dummy", None);
+acc.startup()
+waiter = gensio.waiter(utils.o)
+waiter.service(1)
+acc.shutdown_s()
+del waiter
+del acc
+print("  Success!")
