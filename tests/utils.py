9885004669f551db23f677222569c0fd55087bf8
Corey Minyard <cminyard@mvista.com>
2022-01-21 18:39:27 -0600

    tests:utils: Fix an error report
    It referred to an undefined variable.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- tests/utils.py
+++ tests/utils.py
@@ -696,7 +696,7 @@ def test_shutdown():
     while gensio.gensio_num_alloced() > 0:
         count += 1
         if (count > 100):
-            raise Exception("All gensios were not freed in time" % c)
+            raise Exception("All gensios were not freed in time")
         w.service(1)
     while w.service_now() == 0:
         # Give some time for everyting to clear out.
