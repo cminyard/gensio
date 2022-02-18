e633d83d56b60cfda65ba54dfd1f13b2cc5a09b6
Corey Minyard <cminyard@mvista.com>
2022-02-18 14:08:42 -0600

    c++:swig:go: Add some better examples
    Do the classic telnet client and server.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

---
+++ c++/swig/go/examples/srctelnet_client.go
@@ -0,0 +1,121 @@
+//
+//  gensio - A library for abstracting stream I/O
+//  Copyright (C) 2021  Corey Minyard <minyard@acm.org>
+//
+//  SPDX-License-Identifier: LGPL-2.1-only
+
+// This is an example telnet client in Go.  It reads line-at-a time.
+
+package main
+
+import (
+	"fmt"
+	"github.com/cminyard/go/gensio"
+)
+
+type closeDone struct {
+	gensio.GensioCloseDoneBase
+	ie *ioEvent
+}
+
+func (cd *closeDone) CloseDone() {
+	cd.ie.info.w.Wake()
+	cd.ie.g = nil
+	cd.ie.otherie.g = nil
+	cd.ie.otherie = nil
+	cd.ie = nil
+}
+
+type ioInfo struct {
+	w *gensio.Waiter
+	errstr string
+	inClose bool
+}
+    
+type ioEvent struct {
+	gensio.EventBase
+	g gensio.Gensio
+	otherie *ioEvent
+	info *ioInfo
+}
+
+func (ie *ioEvent) startClose() {
+	if ie.info.inClose {
+		return
+	}
+	ie.info.inClose = true
+	ie.g.SetReadCallbackEnable(false)
+	ie.g.SetWriteCallbackEnable(false)
+	ie.otherie.g.SetReadCallbackEnable(false)
+	ie.otherie.g.SetWriteCallbackEnable(false)
+	cdh := &closeDone{}
+	cdh.ie = ie
+	ie.g.Close(cdh)
+	cdh = &closeDone{}
+	cdh.ie = ie.otherie
+	ie.otherie.g.Close(cdh)
+}
+
+func (ie *ioEvent) Read(err int, data []byte, auxdata []string) uint64 {
+	if err != 0 {
+		ie.info.errstr = gensio.ErrToStr(err)
+		ie.startClose()
+		return 0
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			ie.info.errstr = fmt.Sprintf("%s", r)
+			ie.startClose()
+		}
+	}()
+	count := ie.g.Write(data, nil)
+
+	if count < uint64(len(data)) {
+		ie.g.SetReadCallbackEnable(false)
+		ie.otherie.g.SetWriteCallbackEnable(true)
+	}
+	return count
+}
+
+func (ie *ioEvent) WriteReady() {
+	ie.otherie.g.SetReadCallbackEnable(true)
+	ie.g.SetWriteCallbackEnable(false)
+}
+
+type LogHandler struct {
+	gensio.LoggerBase
+}
+
+func (l *LogHandler) Log(level int, log string) {
+	fmt.Printf("LOG(%s): %s\n", gensio.LogLevelToStr(level), log)
+}
+
+func main() {
+	o := gensio.NewOsFuncs(0, &LogHandler{})
+	w := gensio.NewWaiter(o)
+	info := ioInfo{w, "", false}
+
+	telnetEvent := &ioEvent{}
+	telnetEvent.info = &info;
+
+	userEvent := &ioEvent{}
+	userEvent.info = &info;
+
+	userEvent.g = gensio.NewGensio("stdio(self)", o, userEvent)
+	userEvent.g.OpenS()
+
+	telnetEvent.g = gensio.NewGensio("telnet,tcp,localhost,1234", o,
+		telnetEvent)
+	telnetEvent.g.OpenS()
+
+	userEvent.otherie = telnetEvent
+	telnetEvent.otherie = userEvent
+
+	userEvent.g.SetReadCallbackEnable(true)
+	telnetEvent.g.SetReadCallbackEnable(true)
+
+	w.Wait(2, nil)
+
+	fmt.Println("Shut down due to:", info.errstr);
+}
---
+++ c++/swig/go/examples/srctelnet_server.go
@@ -0,0 +1,132 @@
+//
+//  gensio - A library for abstracting stream I/O
+//  Copyright (C) 2021  Corey Minyard <minyard@acm.org>
+//
+//  SPDX-License-Identifier: LGPL-2.1-only
+
+// This is an example telnet server in Go
+
+package main
+
+import (
+	"fmt"
+	"github.com/cminyard/go/gensio"
+)
+
+type closeDone struct {
+	gensio.GensioCloseDoneBase
+	g gensio.Gensio
+	w *gensio.Waiter
+	se *serverEvent
+}
+
+func (cd *closeDone) CloseDone() {
+	cd.w.Wake()
+	cd.g = nil
+	cd.w = nil
+	cd.se.g = nil
+	cd.se = nil
+}
+
+type serverEvent struct {
+	gensio.EventBase
+	g gensio.Gensio
+	w *gensio.Waiter
+	errstr *string
+}
+
+func (se *serverEvent) startClose() {
+	se.g.SetReadCallbackEnable(false)
+	se.g.SetWriteCallbackEnable(false)
+	cdh := &closeDone{}
+	cdh.g = se.g
+	cdh.w = se.w
+	cdh.se = se
+	se.g.Close(cdh)
+}
+
+func (se *serverEvent) Read(err int, data []byte, auxdata []string) uint64 {
+	if err != 0 {
+		*se.errstr = gensio.ErrToStr(err)
+		se.startClose()
+		return 0
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			*se.errstr = fmt.Sprintf("%s", r)
+			se.startClose()
+		}
+	}()
+	count := se.g.Write(data, nil)
+
+	if count < uint64(len(data)) {
+		se.g.SetReadCallbackEnable(false)
+		se.g.SetWriteCallbackEnable(true)
+	}
+	return count
+}
+
+func (se *serverEvent) WriteReady() {
+	se.g.SetReadCallbackEnable(true)
+	se.g.SetWriteCallbackEnable(false)
+}
+
+type accepterShutdownDone struct {
+	gensio.AccepterShutdownDoneBase
+	w *gensio.Waiter
+	ae *accepterEvent
+}
+
+func (asd *accepterShutdownDone) AccShutdownDone() {
+	asd.ae.acc = nil
+	asd.ae = nil
+	asd.w.Wake()
+}
+
+type accepterEvent struct {
+	gensio.AccepterEventBase
+	acc gensio.Accepter
+	w *gensio.Waiter
+	errstr *string
+}
+
+func (ae *accepterEvent) Log(level int, log string) {
+	fmt.Printf("LOG(%s): %s\n", gensio.LogLevelToStr(level), log)
+}
+
+func (ae *accepterEvent) NewConnection(g gensio.Gensio) {
+	se := &serverEvent{}
+	se.g = g
+	se.w = ae.w
+	se.errstr = ae.errstr
+	g.SetEvent(se)
+	g.SetReadCallbackEnable(true)
+	asd := &accepterShutdownDone{}
+	asd.w = ae.w
+	asd.ae = ae
+	ae.acc.Shutdown(asd)
+}
+
+type LogHandler struct {
+	gensio.LoggerBase
+}
+
+func (l *LogHandler) Log(level int, log string) {
+	fmt.Printf("LOG(%s): %s\n", gensio.LogLevelToStr(level), log)
+}
+
+func main() {
+	o := gensio.NewOsFuncs(0, &LogHandler{})
+	w := gensio.NewWaiter(o)
+	errstr := ""
+	ae := &accepterEvent{}
+	ae.w = w
+	ae.errstr = &errstr
+	ae.acc = gensio.NewAccepter("telnet,tcp,1234", o, ae)
+	ae.acc.Startup()
+
+	w.Wait(2, nil)
+
+	fmt.Println("Shut down due to:", errstr);
+}
