bcc5b1a36b70866cd86e71417d1267cac429422d
Corey Minyard <cminyard@mvista.com>
2022-03-22 12:50:35 -0500

    selector: Remove the current waiter before re-applying the count
    If a waiter returns an error, any count it received is applied to the
    next waiter, making it indepotent if it returns an error.  The code,
    however, didn't remove the first waiter before applying the count, so
    the count would be re-applied to the first waiter.
    
    Move the list removal to before re-applying the count.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- lib/gensio_unix.c
+++ lib/gensio_unix.c
@@ -229,6 +229,8 @@ i_wait_for_waiter_timeout(waiter_t *waiter, unsigned int count,
 	    break;
     }
     timeval_to_gensio_time(timeout, rtv);
+    w.next->prev = w.prev;
+    w.prev->next = w.next;
     if (w.count == 0) {
 	err = 0; /* If our count was decremented to zero, ignore errors. */
     } else if (err) {
@@ -238,8 +240,6 @@ i_wait_for_waiter_timeout(waiter_t *waiter, unsigned int count,
 	 */
 	i_wake_waiter(waiter, count - w.count);
     }
-    w.next->prev = w.prev;
-    w.prev->next = w.next;
     pthread_mutex_unlock(&waiter->lock);
 
     return err;
