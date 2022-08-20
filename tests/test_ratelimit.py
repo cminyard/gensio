d247bf377c74f61fd9e5b121ec7c28c7624b4d6d
Corey Minyard <cminyard@mvista.com>
2022-08-20 16:15:39 -0700

    tests: Add a ratelimit test
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

---
+++ tests/test_ratelimit.py
@@ -0,0 +1,29 @@
+#
+#  gensio - A library for abstracting stream I/O
+#  Copyright (C) 2018  Corey Minyard <minyard@acm.org>
+#
+#  SPDX-License-Identifier: GPL-2.0-only
+#
+
+from utils import *
+import gensio
+
+def do_ratelimit_test(io1, io2, timeout=2000):
+    data = "123456789012345" # 15 characters, 1.5 seconds.
+    io1.handler.set_write_data(data)
+    io2.handler.set_compare(data)
+    if (io1.handler.wait_timeout(1000) != 0):
+        raise Exception(("%s: %s: " % ("do_ratelimit_test", io1.handler.name)) +
+                        ("Sent data too fast"))
+    if (io1.handler.wait_timeout(1000) == 0):
+        raise Exception(("%s: %s: " % ("do_ratelimit_test", io1.handler.name)) +
+                        ("Sent data too slowly"))
+    if (io2.handler.wait_timeout(1000) == 0):
+        raise Exception(("%s: %s: " % ("test_dataxfer", io2.handler.name)) +
+                        ("Read data wasn't received"))
+
+print("Test ratelimit")
+TestAccept(o, "ratelimit(xmit_delay=100m),tcp,localhost,", "tcp,0",
+           do_ratelimit_test, chunksize = 64)
+del o
+test_shutdown()
