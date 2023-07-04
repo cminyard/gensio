de87a9818bc8c392958cb87bdcabb4f5668facde
Corey Minyard <cminyard@mvista.com>
2023-07-04 17:21:08 -0500

    stdsock: Remove an unnecessary check
    gsi always has to be set in this code.
    
    Found by Coverity
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- lib/gensio_stdsock.c
+++ lib/gensio_stdsock.c
@@ -1430,7 +1430,7 @@ gensio_stdsock_close_socket(struct gensio_iod *iod, bool retry, bool force)
 #else
     err = close_socket(o, o->iod_get_fd(iod));
 #endif
-    if (err != GE_INPROGRESS && gsi)
+    if (err != GE_INPROGRESS)
 	o->free(o, gsi);
     return err;
 }
