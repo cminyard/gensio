fab86a28349427d85b02484f2ec604383ded612a
Corey Minyard <cminyard@mvista.com>
2023-08-02 14:49:31 -0500

    test: Rework test script
    Having multiple things waiting has caused issues because if thing happen
    on something you are not waiting for, you don't see it and thus miss it.
    
    Plus, exceptions can be missed in callbacks due to issues in the way
    python works.
    
    This is reworked to have a single event queue where all things go from
    all gensio, acceptors, exceptions, etc.
    
    There is an exception handler added for most callbacks, it will queue
    the exception.
    
    Also use Exception instead of HandlerException, there's no value in
    having a separate exception type.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- tests/test_script.py
+++ tests/test_script.py
@@ -36,19 +36,19 @@ class ScrHandler:
             if self.expect_read_err is not None and err == self.expect_read_err:
                 self.waiter.wake()
             else:
-                raise HandlerException(self.name + ": Got read error: " + err);
+                raise Exception(self.name + ": Got read error: " + err);
             return 0
         i = 0
         while self.expect_read_pos < len(self.expect_read) and i < len(buf):
             if buf[i] != self.expect_read[self.expect_read_pos]:
-                raise HandlerException(self.name + ": data mismatch on byte %d"
+                raise Exception(self.name + ": data mismatch on byte %d"
                                        % self.expect_read_pos);
             i += 1
             self.expect_read_pos += 1
 
         if self.expect_read_pos >= len(self.expect_read):
             if i < len(buf):
-                raise HandlerException(self.name +
+                raise Exception(self.name +
                                        ": Unexpected data from connection")
             self.expect_read = None
             self.waiter.wake()
@@ -67,7 +67,7 @@ class ScrHandler:
 
     def close_done(self, io):
         if not self.expect_close:
-            raise HandlerException(self.name + ": Unexpected close")
+            raise Exception(self.name + ": Unexpected close")
         self.waiter.wake()
         return
 
@@ -77,17 +77,17 @@ class ScrHandler:
 
     def open_done(self, io, err):
         if not self.expect_open:
-            raise HandlerException(self.name + ": Unexpected open")
+            raise Exception(self.name + ": Unexpected open")
         if err is not None:
             if self.expect_open_err is not None:
                 if err != self.expect_open_err:
-                    raise HandlerException(self.name + ": Wrong open error: "
+                    raise Exception(self.name + ": Wrong open error: "
                                            + err)
             else:
-                raise HandlerException(self.name + ": Unexpected open error: "
+                raise Exception(self.name + ": Unexpected open error: "
                                        + err)
         elif self.expect_open_err is not None:
-            raise HandlerException(self.name + ": Expected open error")
+            raise Exception(self.name + ": Expected open error")
             
         if err is None:
             self.io.read_cb_enable(True)
@@ -122,46 +122,46 @@ handlecon.io = con
 con.open(handlecon)
 
 if (handleacc.wait(timeout = 10000000) == 0):
-    raise HandlerException("Timeout waiting for accepter")
+    raise Exception("Timeout waiting for accepter")
 if (handlecon.wait(timeout = 100) != 0):
-    raise HandlerException("Got unexpected open on connection")
+    raise Exception("Got unexpected open on connection")
 
 # The connection should be echoing from echotest
 teststr = "Hi\n"
 handleacc.set_expect_read(teststr)
 handleacc.io.write(teststr, None)
 if (handleacc.wait(timeout = 10000000) == 0):
-    raise HandlerException("Timeout waiting for acc data")
+    raise Exception("Timeout waiting for acc data")
 
 # Now finish the connection successfully
 handlecon.set_expect_open()
 handleacc.set_expect_read("x") # \n doesn't get echoed.
 handleacc.io.write("x\n", None)
 if (handleacc.wait(timeout = 1000) == 0):
-    raise HandlerException("Timeout waiting for acc close data")
+    raise Exception("Timeout waiting for acc close data")
 
 if (handlecon.wait(timeout = 1000) == 0):
-    raise HandlerException("Connection did not open")
+    raise Exception("Connection did not open")
 
 # Transfer some data to make sure that is working
 handleacc.set_expect_read("Test String1")
 handlecon.io.write("Test String1", None)
 if (handleacc.wait(timeout = 1000) == 0):
-    raise HandlerException("Timeout waiting for acc data")
+    raise Exception("Timeout waiting for acc data")
 
 handlecon.set_expect_read("test String2")
 handleacc.io.write("test String2", None)
 if (handlecon.wait(timeout = 1000) == 0):
-    raise HandlerException("Timeout waiting for con data")
+    raise Exception("Timeout waiting for con data")
 
 # Now close it
 handleacc.set_expect_read_err("Remote end closed connection")
 handlecon.set_expect_close()
 con.close(handlecon)
 if (handlecon.wait(timeout = 1000) == 0):
-    raise HandlerException("Timeout waiting for con close")
+    raise Exception("Timeout waiting for con close")
 if (handleacc.wait(timeout = 1000) == 0):
-    raise HandlerException("Timeout waiting for acc close")
+    raise Exception("Timeout waiting for acc close")
 
 del con
 del acc
@@ -187,9 +187,9 @@ handlecon.io = con
 con.open(handlecon)
 
 if (handleacc.wait(timeout = 10000000) == 0):
-    raise HandlerException("Timeout waiting for accepter")
+    raise Exception("Timeout waiting for accepter")
 if (handlecon.wait(timeout = 100) != 0):
-    raise HandlerException("Got unexpected open on connection")
+    raise Exception("Got unexpected open on connection")
 
 # Finish the connection causing the script to return an error
 handlecon.set_expect_open("Local end closed connection")
@@ -197,12 +197,12 @@ handleacc.set_expect_read_err("Remote end closed connection")
 handleacc.set_expect_read("e") # \n doesn't get echoed.
 handleacc.io.write("e\n", None)
 if (handleacc.wait(timeout = 1000) == 0):
-    raise HandlerException("Timeout waiting for acc close data")
+    raise Exception("Timeout waiting for acc close data")
 
 if (handlecon.wait(timeout = 1000) == 0):
-    raise HandlerException("Timeout waiting for con close")
+    raise Exception("Timeout waiting for con close")
 if (handleacc.wait(timeout = 1000) == 0):
-    raise HandlerException("Timeout waiting for acc close")
+    raise Exception("Timeout waiting for acc close")
 
 del con
 del acc
