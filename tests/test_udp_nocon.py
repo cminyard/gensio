55bf4405f58fb7c0d847db06899eb1312a4d71d4
Corey Minyard <cminyard@mvista.com>
2023-08-02 15:29:23 -0500

    tests: Don't test extra info on udp nocon on Windows
    It's not supported.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- tests/test_udp_nocon.py
+++ tests/test_udp_nocon.py
@@ -41,43 +41,45 @@ if h2.wait_timeout(1000) == 0:
 
 # Turn on extrainfo and see that it works.
 
-print("Checking extrainfo in received packdets")
+if not is_windows(): # Windows doesn't have recvmsg, no support
+    print("Checking extrainfo in received packdets")
 
-# Find the loopback address index so we can check it on received packets
-ifs = gensio.ifinfo(o)
-loopidx = 0
-for i in range(0, ifs.get_num_ifs()):
-    flags = ""
-    ifidx = ifs.get_ifindex(i)
-    if ifs.is_up(i):
-        flags = flags + ",UP"
-    if ifs.is_loopback(i):
-        flags = flags + ",LOOPBACK"
-        loopidx = ifidx
-    if ifs.is_multicast(i):
-        flags = flags + ",MULTICAST"
-    if len(flags) > 0:
-        flags = flags[1:]
-    print("  %s(%d): %s" % (ifs.get_name(i), ifidx, flags))
-    for j in range(0, ifs.get_num_addrs(i)):
-        print("    %s/%d" % (ifs.get_addr(i, j), ifs.get_addr_netbits(i, j)))
-del ifs
+    # Find the loopback address index so we can check it on received packets
+    ifs = gensio.ifinfo(o)
+    loopidx = 0
+    for i in range(0, ifs.get_num_ifs()):
+        flags = ""
+        ifidx = ifs.get_ifindex(i)
+        if ifs.is_up(i):
+            flags = flags + ",UP"
+        if ifs.is_loopback(i):
+            flags = flags + ",LOOPBACK"
+            loopidx = ifidx
+        if ifs.is_multicast(i):
+            flags = flags + ",MULTICAST"
+        if len(flags) > 0:
+            flags = flags[1:]
+        print("  %s(%d): %s" % (ifs.get_name(i), ifidx, flags))
+        for j in range(0, ifs.get_num_addrs(i)):
+            print("    %s/%d" % (ifs.get_addr(i, j),
+                                 ifs.get_addr_netbits(i, j)))
+    del ifs
 
-io2.control(gensio.GENSIO_CONTROL_DEPTH_FIRST,
-            gensio.GENSIO_CONTROL_SET,
-            gensio.GENSIO_CONTROL_EXTRAINFO, "1")
+    io2.control(gensio.GENSIO_CONTROL_DEPTH_FIRST,
+                gensio.GENSIO_CONTROL_SET,
+                gensio.GENSIO_CONTROL_EXTRAINFO, "1")
 
-h1.set_compare(cmpstr, auxdata = [ "addr:" + addr2 ])
-io2.write(cmpstr, None)
-if h1.wait_timeout(1000) == 0:
-    raise Exception("test_udp_nocon: read timeout 1")
+    h1.set_compare(cmpstr, auxdata = [ "addr:" + addr2 ])
+    io2.write(cmpstr, None)
+    if h1.wait_timeout(1000) == 0:
+        raise Exception("test_udp_nocon: read timeout 1")
 
-h2.set_compare(cmpstr, auxdata = [ "addr:" + addr1,
-                                   "ifidx:%d" % loopidx,
-                                   "daddr:ipv4,127.0.0.1" ])
-io1.write(cmpstr, [ "addr:" + addr2 ])
-if h2.wait_timeout(1000) == 0:
-    raise Exception("test_udp_nocon: read timeout 2")
+    h2.set_compare(cmpstr, auxdata = [ "addr:" + addr1,
+                                       "ifidx:%d" % loopidx,
+                                       "daddr:ipv4,127.0.0.1" ])
+    io1.write(cmpstr, [ "addr:" + addr2 ])
+    if h2.wait_timeout(1000) == 0:
+        raise Exception("test_udp_nocon: read timeout 2")
 
 io_close((io1, io2))
 del io1
