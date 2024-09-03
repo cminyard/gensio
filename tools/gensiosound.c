17e69565c0c4caf0082e21983c4b507d0337af9c
Corey Minyard <corey@minyard.net>
2024-09-03 10:44:53 -0500

    tools:sound: Get rid of some unnecessary assignment
    Getting version and help don't need a return value set.
    
    Signed-off-by: Corey Minyard <corey@minyard.net>

--- tools/gensiosound.c
+++ tools/gensiosound.c
@@ -247,10 +247,10 @@ main(int argc, char *argv[])
 	    list_devs = true;
 	} else if ((rv = cmparg(argc, argv, &arg, "-d", "--debug", NULL))) {
 	    gensio_set_log_mask(GENSIO_LOG_MASK_ALL);
-	} else if ((rv = cmparg(argc, argv, &arg, NULL, "--version", NULL))) {
+	} else if (cmparg(argc, argv, &arg, NULL, "--version", NULL)) {
 	    printf("Version %s\n", gensio_version_string);
 	    exit(0);
-	} else if ((rv = cmparg(argc, argv, &arg, "-h", "--help", NULL))) {
+	} else if (cmparg(argc, argv, &arg, "-h", "--help", NULL)) {
 	    help(0);
 	} else {
 	    fprintf(stderr, "Unknown argument: %s, us -h for help\n",
