e1337fb6491085dabf68edd344f12d60108e8008
Corey Minyard <corey@minyard.net>
2025-05-14 14:30:24 -0700

    Fix control number for drain timeout
    Set it so it doesn't conflict with anything else.
    
    Signed-off-by: Corey Minyard <corey@minyard.net>

--- include/gensio/gensio_control.h
+++ include/gensio/gensio_control.h
@@ -76,7 +76,7 @@
 #define GENSIO_CONTROL_SER_SEND_MODEMSTATE	53u
 #define GENSIO_CONTROL_SER_SEND_LINESTATE	54u
 
-#define GENSIO_CONTROL_DRAIN_TIMEOUT		45u
+#define GENSIO_CONTROL_DRAIN_TIMEOUT		55u
 
 /* Keep the async control number in a different range, just to be safe. */
 #define GENSIO_ACONTROL_SER_BAUD		1000u
