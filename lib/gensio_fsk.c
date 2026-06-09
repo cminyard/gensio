cminyard/gensio

commit 7135ddb13b71a1c7c29621f297fa96ac3c4a199b

fsk: Clear out the output buffer at the end of a packet
Otherwise the last little bit might not be transmitted and the close
operation can hang.

Signed-off-by: Corey Minyard <corey@minyard.net>

lib/gensio_fsk.c
@@ -1122,6 +1122,13 @@ fsk_ul_write(struct gensio_filter *filter,
  out_process:
     fsk_handle_send(sfilter, handler, cb_data);
  out:
+    /*
+     * Make sure to clear the output buffer if there's anything left
+     * and we can still write.  Otherwise the base handler won't know
+     * we have data left to send.
+     */
+    if (sfilter->xmit_buf_len > 0)
+	fsk_send_buffer(sfilter, handler, cb_data);
     fsk_unlock(sfilter);
     if (!rv && rcount)
 	*rcount = count;

commit 1b984150f8c3ddb4a5a7c6f68ea26511775155a4

fsk: Fix checking end of buffer on write
There was a missing check, exacerbated by the fix below.  Check
for end of data in the write function.

Fixes: 7135ddb13b71a("fsk: Clear out the output buffer at the end of a packet")
Signed-off-by: Corey Minyard <corey@minyard.net>

lib/gensio_fsk.c
@@ -1127,8 +1127,13 @@ fsk_ul_write(struct gensio_filter *filter,
      * and we can still write.  Otherwise the base handler won't know
      * we have data left to send.
      */
-    if (sfilter->xmit_buf_len > 0)
+    if (!rv && sfilter->xmit_buf_len > 0)
 	fsk_send_buffer(sfilter, handler, cb_data);
+    if (!rv && sfilter->xmit_buf_len == 0)
+	/*
+	 * No more data to send, start the timer now.
+	 */
+	fsk_start_drain_timer(sfilter);
     fsk_unlock(sfilter);
     if (!rv && rcount)
 	*rcount = count;